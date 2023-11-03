3f0feb27c5701eb6fce9c666aab461a62014a5d8
SwaminathanT02 <swaminathant02@gmail.com>
2023-11-03 21:39:10 +0530

    Added Simon Game

--- index.js
+++ index.js
@@ -1,77 +1,136 @@
-// SET COLOR FOR 'software' ON MOUSE HOVER
-var currentColor = document.getElementsByTagName('code')[0].style.color;
-
-document.querySelector('code').addEventListener("mouseover", function () {
-    document.getElementsByTagName('code')[0].style.color = "blue";
-});
-document.querySelector('code').addEventListener("mouseout", function () {
-    document.getElementsByTagName('code')[0].style.color = currentColor;
-});
-
-
 // DRUM GAME
+var gameStarted = false;
 
+$(".drum-start-button").click(function() {
+    gameStarted = !gameStarted;
+    $(this).text(gameStarted ? "Stop" : "Play");
+});
 
-var drumClassLength = document.querySelectorAll(".drum").length;
-for(var i = 0; i < drumClassLength; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        var buttonPressed = this.innerHTML;
+$(".drum").click(function() {
+    if (gameStarted) {
+        var buttonPressed = $(this).text();
         makeSound(buttonPressed);
-        animatePress(buttonPressed);
-    });
-}
+        drumAnimatePress(buttonPressed);
+    }
+});
 
-document.addEventListener("keydown", function(event){
-    makeSound(event.key);
-    animatePress(event.key);
+$(document).keydown(function(event) {
+    if (gameStarted) {
+        var buttonPressed = event.key.toLowerCase(); // Convert to lowercase to handle both cases
+        makeSound(buttonPressed);
+        drumAnimatePress(buttonPressed);
+    }
 });
 
 function makeSound(buttonPressed) {
     switch(buttonPressed){
         case "w":
-            var tom1 = new Audio("./sounds/tom-1.mp3");
-            tom1.play();
+            playSound("drum-sounds", "tom-1.mp3");
             break;
-        
         case "a":
-            var tom2 = new Audio("./sounds/tom-2.mp3");
-            tom2.play();
+            playSound("drum-sounds", "tom-2.mp3");
             break;
-        
         case "s":
-            var tom3 = new Audio("./sounds/tom-3.mp3");
-            tom3.play();
+            playSound("drum-sounds", "tom-3.mp3");
             break;
-
         case "d":
-            var tom4 = new Audio("./sounds/tom-4.mp3");
-            tom4.play();
+            playSound("drum-sounds", "tom-4.mp3");
             break;
-        
         case "j":
-            var snare = new Audio("./sounds/snare.mp3");
-            snare.play();
+            playSound("drum-sounds", "snare.mp3");
             break;
-        
         case "k":
-            var crash = new Audio("./sounds/crash.mp3");
-            crash.play();
-            break;
-        
+            playSound("drum-sounds", "crash.mp3")
+            break;           
         case "l":
-            var kick = new Audio("./sounds/kick-bass.mp3");
-            kick.play();
+            playSound("drum-sounds", "kick-bass.mp3")
             break;
-
         default:
             console.log(this.innerHTML);
     }
 }
 
-function animatePress(buttonPressed){
-    var currentClassList = document.querySelector("." + buttonPressed).classList;
-    currentClassList.add("pressed");
-    setTimeout(function() {
-        currentClassList.remove("pressed");
+function drumAnimatePress(buttonPressed){
+    if (gameStarted) {
+        var currentClassList = $("." + buttonPressed).addClass("pressed");
+        setTimeout(function() {
+            currentClassList.removeClass("pressed");
+        }, 100);
+    }
+}
+
+
+
+// SIMON GAME
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var simonStarted = false;
+
+$(".simon-start-button").click(function() {
+    if(!simonStarted){
+        $("#simon-level-title").text("Level " + level);
+        nextSequence();
+        simonStarted = true;
+    }
+});
+
+$(".simon-btn").on("click", function(event){
+    if(simonStarted){
+        var userChosenColour = $(this).attr("id").split("-")[1];
+        userClickedPattern.push(userChosenColour);
+        playSound("simon-sounds", userChosenColour + ".mp3");
+        simonAnimatePress(userChosenColour);
+        checkAnswer(userClickedPattern.length - 1);
+    }
+});
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#simon-level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $(".simon-" + randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound("simon-sounds", randomChosenColour + ".mp3");
+}
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function () {
+              nextSequence();
+            }, 1000);
+          }
+    }
+    else{
+        playSound("simon-sounds", "wrong.mp3");
+        var currentBGColour = $('.simon-container').css("background-color");
+        $('.simon-container').css("background-color", "red");
+        setTimeout(function(){
+            $('.simon-container').css("background-color", currentBGColour);
+        }, 200);
+        $("#simon-level-title").text("Game Over! Press 'Play' to Restart!");
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    simonStarted = false;
+}
+
+function playSound(directory, name){
+    var currentAudio = new Audio("./sounds/" + directory + "/" + name);
+    currentAudio.play();
+}
+
+function simonAnimatePress(currentColour){
+    $("#simon-" + currentColour).addClass("simon-pressed");
+    setTimeout(function(){
+        $("#simon-" + currentColour).removeClass("simon-pressed");
     }, 100);
 }
